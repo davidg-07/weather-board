0cfb2703f0472d9e442f073c757a46f6c2c6ad7c
David Galvez <david.dmg17@gmail.com>
2022-06-19 11:30:35 -0700

    get data from url

--- assests/script.js
+++ assests/script.js
@@ -2,17 +2,24 @@ const searchBtnEl = document.getElementById("btn-search");
 const inputEl = document.getElementById('input-city');
 const apikey = '7c076ebdc9171cc941023949cd3161e3'
 
-searchBtnEl.addEventListener('click', function(){   
+searchBtnEl.addEventListener('click', function () {
     console.log('hello all' + inputEl.value);
-    // const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}`;
+
     getGeoLocation(inputEl.value)
 });
 
 function getGeoLocation(cityname) {
-    const apiurl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${apikey}`;
-    fetch(apiurl).then(function(res){
-        return res.json();
-    }).then(function(data){
-        console.log('data: ', data)
-    }); 
+    const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${apikey}`;
+    fetch(geoUrl)
+        .then(res => res.json())
+        .then(geoData => {
+            console.log('data: ', geoData);
+            const lat = geoData[0].lat;
+            const lon = geoData[0].lon;
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}`;
+            fetch(weatherUrl)
+                .then(res => res.json())
+                .then(data => console.log(data));
+        })
+        .catch(err => console.error(err));
 }
